f5ecf09d5f503735a013aa0494fb784fd51ed155
wangxueping03 <wangxueping03@kuaishou.com>
2025-02-24 09:21:05 +0800

    * 第一章

--- myCppTest/myCppTest/myCppTest.cpp
+++ myCppTest/myCppTest/myCppTest.cpp
@@ -1,11 +1,44 @@
-// myCppTest.cpp : ¶¨Òå¿ØÖÆÌ¨Ó¦ÓÃ³ÌĞòµÄÈë¿Úµã¡£
+// myCppTest.cpp : ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ì¨Ó¦ï¿½Ã³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Úµã¡£
 //
 
+#include <iostream>
 #include "stdafx.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
+// int _tmain(int argc, _TCHAR* argv[])
+// {
+// 	return 0;
+// }
+
+
+// è·å–C++ç‰ˆæœ¬
+long getCPPStandard()
 {
-	return 0;
+    // Visual Studio is non-conforming in support for __cplusplus (unless you set a specific compiler flag, which you probably haven't)
+    // In Visual Studio 2015 or newer we can use _MSVC_LANG instead
+    // See https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
+#if defined (_MSVC_LANG)
+    return _MSVC_LANG;
+#elif defined (_MSC_VER)
+    // If we're using an older version of Visual Studio, bail out
+    return -1;
+#else
+    // __cplusplus is the intended way to query the language standard code (as defined by the language standards)
+    return __cplusplus;
+#endif
 }
 
+void foo(int a){}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+    
+    unsigned int a = 10;
+    foo(a); // ç±»å‹å¼ºåˆ¶è½¬æ¢
+
+    int width = 5.0;
+    // int width1 { 5.0 };  // ç¦æ­¢éšå¼è½¬æ¢
+    
+    return 0;
+}
